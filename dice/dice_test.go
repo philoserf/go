06418225b063cc3a2b60ef8ec2baa768b9506451
Mark Ayers <mark@philoserf.com>
2024-08-19 12:20:18 -0400

    fixup

--- dice/dice_test.go
+++ dice/dice_test.go
@@ -20,9 +20,9 @@ func TestD(t *testing.T) {
 		{"D100 is between 1 and 100", 100, 1},
 	}
 	for _, test := range tests {
-		test := test
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
+
 			for range [1000]int{} {
 				got := dice.D(test.upper)
 				if !(got <= test.upper) || !(got >= test.lower) {
